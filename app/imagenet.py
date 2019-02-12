8db35277a55e6a777b488f0b8e1ae0af3cdea7b8
Tomiyama <10380r@gmail.com>
2019-02-12 10:31:07 +0900

    fix

--- app/imagenet.py
+++ app/imagenet.py
@@ -5,16 +5,14 @@ import numpy as np
 import sys
 
 def imagenet(file):
-
-    # モデルの設定
     model = VGG16(weights='imagenet')
     # 入力するデータを読み込み整形
-    img = image.load_img(filename, target_size=(224, 224))
+    img = image.load_img(file, target_size=(224, 224))
     # 画像を配列に変換
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     # モデルにかけて推論
     preds = model.predict(preprocess_input(x))
     #推論結果
-    results = decode_predictions(preds, top=5)[0]
+    results = decode_predictions(preds)[0]
     return results
