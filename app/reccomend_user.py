81c2cecb7489ee9a6f6dbfff6c2c656ae4e79769
Tomiyama <10380r@gmail.com>
2019-02-18 01:51:42 +0900

    [wip]ユーザーのレコメンド関数を作成

--- app/reccomend_user.py
+++ app/reccomend_user.py
@@ -1,25 +1,40 @@
 import numpy as np
-import pickle
 import pandas as pd
-import django
-import os,sys
+import pickle,django,os,sys
 
-def get_user(file):
-    with open(file, 'rb') as f:
-        print(pickle.load(f))
+def get_pkl(user):
+    with open('pickles/%s.pkl' %(user), 'rb') as f:
+        return pickle.load(f)
 
 def sim_cos(user1, user2):
     cos = np.dot(user1, user2.T) / (np.linalg.norm(user1) * np.linalg.norm(user2))
     return cos
 
-def main():
-    # ローカルでmodelを扱えるようにする
+def get_users():
+     # ローカルでmodelを扱えるようにする
     sys.path.append('/Users/tommy/src/develop/mimicstagram/mimicstagram/')
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mimicstagram.settings')  # 自分のsettings.py
     django.setup()
     from app.models import User
-    for user in User.objects.all():
-        print(user)
+    users = [user for user in User.objects.all()]
+    return users 
 
-if __name__ =='__main__':
-    main()
+def reccomend_user(me)):
+    my_dict  = get_pkl(me)
+    my_array = np.fromiter(my_dict.values(), dtype=float)
+    users    = get_users()
+    most_sim      = 0
+    most_sim_user = ''
+    for user in users:
+        if user == me:
+            continue
+        else:
+            user_dict = get_pkl(user)
+            user_array = np.fromiter(user_dict.values(), dtype=float)
+
+            similarity = sim_cos(my_array, user_array)
+            if most_sim < similarity:
+                most_sim = similarity
+                most_sim_user = user
+
+    return most_sim, most_sim_user
