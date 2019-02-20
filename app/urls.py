17e8e21d6c8b8ee1532d0f69b16c4e3ee077c768
Tomiyama <10380r@gmail.com>
2019-02-21 01:41:58 +0900

    ユーザのview製作開始

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
         path('',          views.index,  name='index'),
         path('post',      views.post,   name='post'),
+        path('user_detail/<int:pk>/', views.UserDetail.as_view(), name='user_detail'),
         path('recommend', views.recommend,   name='recommend'),
         path('like/<int:like_id>',   views.like, name='like'),
         ]
